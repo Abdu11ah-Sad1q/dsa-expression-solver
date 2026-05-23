c28250dbf712c73aae75a477c18aeb42e1f81a77
Abdu11ah-Sad1q <abdullahsadiq774@gmail.com>
2026-05-24 01:30:17 +0500

    Feature: Add shunting-yard algorithm for infix-to-postfix token conversion

--- main.cpp
+++ main.cpp
@@ -34,6 +34,110 @@ bool matches(char open, char close) {
            (open == '[' && close == ']') || 
            (open == '{' && close == '}');
 }
-int main(){
-  return 0;
+
+// Converts standard expression to Reverse Polish Notation (Tokens)
+vector<string> convertInfixToPostfix(const string& infix, vector<string>& variables) {
+    stack<char> operatorStack; 
+    vector<string> postfixTokens; 
+    int i = 0;
+    int len = infix.length();
+
+    while (i < len) {
+        if (isspace(infix[i])) {
+            i++;
+            continue;
+        }
+
+        // Grabbing multi-digit whole numbers
+        if (isdigit(infix[i])) {
+            string num = "";
+            while (i < len && isdigit(infix[i])) {
+                num += infix[i];
+                i++;
+            }
+            postfixTokens.push_back(num);
+            continue;
+        }
+
+        // Grabbing legal C++ variable names (allows letters/underscores/digits)
+        if (isalpha(infix[i]) || infix[i] == '_') {
+            string var = "";
+            while (i < len && (isalnum(infix[i]) || infix[i] == '_')) {
+                var += infix[i];
+                i++;
+            }
+            postfixTokens.push_back(var);
+            // Track uniquely found variables to query them later
+            if (find(variables.begin(), variables.end(), var) == variables.end()) {
+                variables.push_back(var);
+            }
+            continue;
+        }
+
+        if (isOpeningBracket(infix[i])) {
+            operatorStack.push(infix[i]);
+            i++;
+            continue;
+        }
+
+        if (isClosingBracket(infix[i])) {
+            bool foundMatch = false;
+            while (!operatorStack.empty()) {
+                char topOp = operatorStack.top();
+                if (isOpeningBracket(topOp)) {
+                    if (matches(topOp, infix[i])) {
+                        foundMatch = true;
+                        operatorStack.pop();
+                    }
+                    break;
+                }
+                postfixTokens.push_back(string(1, topOp));
+                operatorStack.pop();
+            }
+            if (!foundMatch) {
+                cerr << "Syntax Error: Mismatched grouping symbols.\n";
+                exit(1); // Syntax error exit code
+            }
+            i++;
+            continue;
+        }
+
+        if (isOperator(infix[i])) {
+            while (!operatorStack.empty() && !isOpeningBracket(operatorStack.top()) &&
+                   getPrecedence(operatorStack.top()) >= getPrecedence(infix[i])) {
+                postfixTokens.push_back(string(1, operatorStack.top()));
+                operatorStack.pop();
+            }
+            operatorStack.push(infix[i]);
+            i++;
+            continue;
+        }
+
+        cerr << "Syntax Error: Illegal character in expression.\n";
+        exit(1);
+    }
+
+    // Dump out remaining stacked operators
+    while (!operatorStack.empty()) {
+        char topOp = operatorStack.top();
+        if (isOpeningBracket(topOp)) {
+            cerr << "Syntax Error: Unclosed grouping bracket detected.\n";
+            exit(1);
+        }
+        postfixTokens.push_back(string(1, topOp));
+        operatorStack.pop();
+    }
+
+    return postfixTokens;
+}
+
+int main() {
+    string infixExpression;
+    cerr << "Enter the expression: ";
+    if (!getline(cin, infixExpression)) {
+        return 0;
+    }
+    vector<string> variables;
+    vector<string> postfixTokens = convertInfixToPostfix(infixExpression, variables);
+    return 0;
 }
